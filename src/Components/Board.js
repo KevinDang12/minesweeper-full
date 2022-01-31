875e9a21c00d46961ac553a05f596fd00de2551e
Kevin Dang <kevindouglasdang@gmail.com>
2022-01-30 22:24:07 -0500

    First attempt on minesweeper game

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -13,19 +13,79 @@ const styles = {
     }
 }
 
+let board;
+const boardSize = 5;
+
 export class Board extends Component {
 
-    displayBoard = () => {
-        let j, i;
-        let rows = [];
-        let board = [];
+    // state = {
+    //     adjacentMines: 0
+    // }
 
-        for (j = 0; j < 10; j++) {
-            rows.push(<Tile/>)
+    checkAdjacent = (tile) => {
+
+        const area = [
+            -1, -1,
+            -1, 0,
+            -1, 1,
+            0, -1,
+            0, 1,
+            1, -1,
+            1, 0,
+            1, 1,
+        ]
+
+        let adjacent = [];
+        let tileX = tile.props.x;
+        let tileY = tile.props.y;
+
+        for (let i = 0; i < area.length; i += 2) {
+            let adjacentX = area[i];
+            let adjacentY = area[i + 1];
+
+            let currentX = adjacentX + tileX;
+            let currentY = adjacentY + tileY;
+
+            if (currentX >= 0 && currentY >= 0 && currentX < boardSize && currentY < boardSize) {
+                adjacent.push(board[currentX][currentY]);
+            }
         }
 
-        for (i = 0; i < 10; i++) {
-            board.push(<div style={styles.board}>{rows}</div>);
+        return adjacent;
+    }
+
+    numOfAdjacentMines = (adjacent) => {
+        let count = 0;
+        for (const tile of adjacent) {
+            if (tile.props.hasMine) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    displayBoard = () => {
+        let x, y;
+        let rows = [];
+        board = [];
+
+        for (y = 0; y < boardSize; y++) {
+            for (x = 0; x < boardSize; x++) {
+                let mine = Math.random() < 0.25;
+                let tile = <Tile x={x} y={y} hasMine={mine}/>;
+                rows.push(tile);
+            }
+            board.push(rows);
+            rows = [];
+        }
+
+        for (x = 0; x < boardSize; x++) {
+            for (y = 0; y < boardSize; y++) {
+                const numOfMines = this.numOfAdjacentMines(this.checkAdjacent(board[x][y]));
+                console.log(board[x][y].props.adjacentMines);
+                // board[x][y].setState({adjacentMines: numOfMines});
+                // board[x][y].setState({numOfAdjacentMines: numOfMines});
+            }
         }
 
         return board;
--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -1,38 +1,59 @@
 import React, {Component} from 'react';
+import one from '../1.png';
+import two from '../2.png';
+import three from '../3.png';
+import four from '../4.png';
+import five from '../5.png';
+import six from '../6.png';
+import seven from '../7.png';
+import eight from '../8.png';
+import mine from '../bomb.png';
+import flag from '../flag.png';
 
 /*
- * OnClick change button to white
- * Right Click on button:
- * - Add flag
- * - remove flag
- * - can be done on gray button
+ * Change the adjacent mines value on set up
  */
 
 export class Tile extends Component {
 
     state = {
-        color: 'rgba(161,160,160,0.8)',
-        value : "",
-        click: false
+        color: 'rgb(161,160,160)',
+        click: false,
+        hasMine: this.props.hasMine,
+        adjacentMines: 0,
+        x: this.props.x,
+        y: this.props.y,
+        image: null
+    }
+
+    changeMine = (value) => {
+        this.setState({adjacentMines: value});
     }
 
     changeColor = () => {
-        console.log('Left click');
-        if (this.state.value === "") {
+        if (this.state.hasMine) {
+            this.setState({color: 'rgb(255,0,0)'});
+            this.setState({image: mine});
+            this.setState({click: true});
+
+        } else if (this.state.image !== flag) {
             this.setState({color: 'rgb(255,255,255)'});
             this.setState({click: true});
         }
     }
 
+    testText () {
+        console.log("test");
+    }
+
     flag = (e) => {
         e.preventDefault();
         if (this.state.click === false) {
-            if (this.state.value === "F") {
-                this.setState({value: ""});
-                console.log("none");
-            } else if (this.state.value === "") {
-                this.setState({value: "F"});
-                console.log("Flag");
+            if (this.state.image === flag) {
+                this.setState({image: one}); // index of hasMine - 1 from image array
+
+            } else if (this.state.value !== flag) {
+                this.setState({image: flag});
             }
         }
     }
@@ -48,7 +69,8 @@ export class Tile extends Component {
                     backgroundColor: this.state.color,
                     margin: '0.5px'}}
                 onClick={this.changeColor}>
-                {this.state.value}
+                <img src = {this.state.image}/>
+                {/*{this.state.value}*/}
             </button>
         );
     }
